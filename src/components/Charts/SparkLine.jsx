b1a10ada23099f56e87adc5b6537d66e2a65feaa
theUyonator <duyo@ualberta.ca>
2022-10-08 09:01:44 -0600

    Implemented sortable Orders page

--- src/components/Charts/SparkLine.jsx
+++ src/components/Charts/SparkLine.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
+import React from 'react';
+import { SparklineComponent, Inject, SparklineTooltip } from '@syncfusion/ej2-react-charts';
 
-const SparkLine = () => {
+const SparkLine = ({ id, height, width, color, data, type, currentColor }) => {
     return (
-        <div>
-            
-        </div>
+       <SparklineComponent 
+        id={id}
+        height={height}
+        width={width}
+        lineWidth={1}
+        valueType="Numeric"
+        fill={color}
+        border={{ color: currentColor, width: 2 }} 
+        dataSource={data}
+        xName="x"
+        yName="yval"
+        type={type}
+        tooltipSettings={{
+            visible: true,
+            format: '${x} : data ${yval}',
+            trackLineSettings: {
+                visible: true
+            },
+        }}
+        markerSettings={{ visible: ['All'], size: 2.5, fill: currentColor }}
+       >
+           <Inject services={[SparklineTooltip]} />
+       </SparklineComponent>
     )
 }
 
